fd85d55944b87e31aa5070b090ca1d7a3a9db384
teofanis <47872542+teofanis@users.noreply.github.com>
2020-11-27 23:56:51 +0000

    input

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,10 @@ import { PageLayout } from '../common/PageLayout';
 export default function Home() {
     return (
         <PageLayout>
-            Home
+            <h1>
+                Home
+           </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur lacus non diam sodales pretium. Aliquam sodales nisl molestie sem tempor lacinia. Mauris iaculis velit a ullamcorper bibendum. Aenean sed congue sem. Phasellus lobortis bibendum dui, non lacinia neque auctor at. Duis ante lorem, porttitor vitae efficitur consectetur, accumsan et odio. Sed vel sapien a justo luctus ornare vel ac augue. Quisque eros arcu, pretium vitae accumsan non, iaculis et lacus. Aliquam porttitor nisi sit amet urna auctor mattis. Mauris hendrerit, risus sed efficitur egestas, massa augue luctus est, sit amet finibus diam massa nec mi. Phasellus sodales diam a mauris venenatis, nec aliquam ligula interdum. Ut id dui vel libero tempor pharetra. Fusce vulputate eget sem quis bibendum.</p>
         </PageLayout>
     )
 
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import { PageLayout } from '../common/PageLayout';
+import React, {useState} from "react";
+import { PageLayout, Input } from "../common";
+import styled from "styled-components";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  color: black;
+  border-radius: 4px;
+`;
 
 export default function Login() {
-    return (
+    const [formFields, setFormFields] = useState({ username: '', password: '' });
+    function handleInputChange(e) {
+        e.persist();
+        setFormFields(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    }
+  return (
     <PageLayout>
-        Login
+      <h1>Login</h1>
+          <Form>
+              <Input
+                  value={formFields.username}
+                  onChange={handleInputChange}
+                  name="username"
+                  placeholder="Username" />
+              <Input
+                  value={formFields.password}
+                  onChange={handleInputChange}
+                  name="password"
+                  type="password"
+                  placeholder="Password" />
+      </Form>
     </PageLayout>
-    )
-
+  );
 }
