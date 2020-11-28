c381190f73e510557e9c385a20230f96886ab25d
Teofanis Papadopulos <teos_97@hotmail.com>
2020-11-28 23:45:19 +0000

    dark/light implements

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -25,7 +25,7 @@ const Menu = styled.nav`
   padding: 8px;
   box-sizing: border-box;
   border-bottom: 3px solid ${p => p.theme.secondaryColor};
-  background:white;
+  background: ${p => p.theme.bodyBackgroundColor};
 
   @media (min-width: 768px) {
     display: flex;
@@ -51,7 +51,7 @@ const StyledLink = styled(Link)`
   box-sizing: border-box;
   margin: auto 0;
   font-weight: ${(p) => (p.isActive ? "bold" : "normal")};
-  color: black;
+  color: ${p => p.theme.bodyFontColor};
 `;
 
 const MobileMenuIcon = styled.div`
@@ -62,7 +62,7 @@ const MobileMenuIcon = styled.div`
 
     >div{
         height:3px;
-        background:black;
+        background:${p => p.theme.bodyFontColor};
         margin:5px 0;
         width:100%;
     }
